b0e3b8e060a440667193569591f7104f8ad103e1
Robert Sturla <robertsturla@outlook.com>
2025-08-30 23:32:02 +0100

    feat(renovate): create shared Renovate configuration files
    Create shared Renovate configuration files, to be used by all repositories
    in the bootc-dev org.
    
    Some of the configuration has been inspired by the RedHat
    [github.com/platform-engineering-org/.github](https://github.com/platform-engineering-org/.github)
    configuration repository.
    
    Signed-off-by: Robert Sturla <robertsturla@outlook.com>

--- renovate-config.js
+++ renovate-config.js
@@ -1,21 +1,29 @@
 module.exports = {
+  // Find all repositories the GitHub App token has permissions to
   autodiscover: true,
-  branchPrefix: 'test-renovate/',
+
+  // Don't create the onboarding PRs
+  //
+  // All repositories in the organisation will inherit the shared configuration
+  // (./renovate-shared-config.json) by default unless they opt-out.
+  onboarding: false,
+
+  // Centralise all Renovate configuration into this repository
+  //
+  // This allows for easier management of Renovate settings across multiple
+  // repositories.  Each individual repository can still contain their own
+  // configuration.
+  inheritConfig: true,
+  inheritConfigRepoName: '{{parentOrg}}/infra',
+  inheritConfigFileName: "renovate-shared-config.json",
+  inheritConfigStrict: true,
+
+  // Prefix all branches created by Renovate with "bootc-renovate/"
+  branchPrefix: 'bootc-renovate/',
+
+  // Configure Renovate to use GitHub-specific API calls
   platform: 'github',
+
+  // Enable dependency updates on forked repositories in the organisation
   forkProcessing: 'enabled',
-  packageRules: [
-    {
-      description: 'lockFileMaintenance',
-      matchUpdateTypes: [
-        'pin',
-        'digest',
-        'patch',
-        'minor',
-        'major',
-        'lockFileMaintenance',
-      ],
-      dependencyDashboardApproval: false,
-      minimumReleaseAge: '0 days',
-    },
-  ],
 };
