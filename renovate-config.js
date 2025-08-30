e71331b8af6417c829608131f0e911d602d3d87e
Colin Walters <walters@verbum.org>
2025-08-30 10:11:05 -0400

    update config

--- renovate-config.js
+++ renovate-config.js
@@ -3,10 +3,8 @@ module.exports = {
   branchPrefix: 'test-renovate/',
   username: 'renovate-release',
   gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
-  onboarding: false,
   platform: 'github',
   forkProcessing: 'enabled',
-  dryRun: 'full',
   packageRules: [
     {
       description: 'lockFileMaintenance',
